8386cbfbff5413451877aecceb4efd428df01c4b
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-20 16:56:30 +0300

    count down before execution

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown int = 10
+    while int >= 1
+    puts"#{int} SECOND(S)!"
+    int -= 1
+    #   binding.pry
+end
+"HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep int
+    sleep(int)
+    countdown
 end
